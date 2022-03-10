d4e830d6d5fed5967d797495da16e8f1d8f61d7a
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2022-03-09 19:09:05 -0800

    more functionality

--- kuspit/kuspit_tracker.py
+++ kuspit/kuspit_tracker.py
@@ -1,4 +1,5 @@
 import base64
+import pandas as pd
 
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
@@ -7,6 +8,9 @@ from quickstart import get_credentials
 
 
 TICKER_KEY = "Emisora"
+OPERATION_TYPE = "Tipo de Operación"
+UNIT_PRICE = "Precio Unitario"
+TITLE_ASSIGNED = "Títulos Asignados"
 
 
 creds = get_credentials()
@@ -31,6 +35,9 @@ def parse_order_details(decoded_data):
     soup = BeautifulSoup(decoded_data, "lxml")
     table = soup.find("table", {
                       "width": "515"})
+    if not table:
+        return {}
+
     table_data = table.find_all("td")
 
     def clean_string(string):
@@ -44,7 +51,8 @@ def parse_order_details(decoded_data):
 
 def get_order_details_in_messages(message_list):
     order_details = []
-    message_ids = [message.get("id") for message in message_list[:10]]
+    messages = []
+    message_ids = [message.get("id") for message in message_list]
 
     messages = [gmail.users().messages().get(userId='me', id=message_id, format="full").execute()
                 for message_id in message_ids]
@@ -77,7 +85,57 @@ def get_tickers_count_from_orders(orders):
     return tickers_count
 
 
+def group_ticker_operations_in_orders(ticker, orders):
+    ticker_orders = list(filter(lambda x: x.get(TICKER_KEY) == ticker, orders))
+    result = {
+        TICKER_KEY: ticker,
+    }
+
+    def parse_price(price_str):
+        return float(price_str.replace('$ ', "").replace(',', ""))
+
+    for operation in ticker_orders:
+        result[operation.get(OPERATION_TYPE)] = result.get(
+            operation.get(OPERATION_TYPE), 0) + parse_price(operation.get("Importe"))
+
+
+def operations_in_orders_by_ticker(orders):
+    tickers = get_tickers_count_from_orders(orders).keys()
+    operations_per_ticker = {}
+
+    for ticker in tickers:
+        operations_per_ticker[ticker] = list(
+            filter(lambda x: x.get(TICKER_KEY) == ticker, orders))
+
+    return operations_per_ticker
+
+
+def format_operations_in_orders_by_ticker(operations_in_orders):
+    for ticker in operations_in_orders:
+        print(f"{ticker}\n{'-'*50}")
+        for operation in operations_in_orders[ticker]:
+            print(
+                f'{operation.get(OPERATION_TYPE)} -- {operation.get("Fecha de Asignación")} -- {operation.get(UNIT_PRICE)} -- {operation.get(TITLE_ASSIGNED)}')
+
+        print("-"*50)
+
+
 orders = get_order_details_in_messages(
-    get_all_messages("Orden de compra de Acciones."))
+    get_all_messages("kuspit orden de compra de acciones "))
 
 print(get_tickers_count_from_orders(orders))
+group_ticker_operations_in_orders("FUNO", orders)
+
+# format_operations_in_orders_by_ticker(operations_in_orders_by_ticker(orders))
+df = pd.DataFrame(orders)
+df.to_csv("orders_in_mail_compra.csv")
+
+orders = get_order_details_in_messages(
+    get_all_messages("kuspit orden de venta de acciones "))
+
+print(get_tickers_count_from_orders(orders))
+group_ticker_operations_in_orders("FUNO", orders)
+
+# format_operations_in_orders_by_ticker(operations_in_orders_by_ticker(orders))
+df = pd.DataFrame(orders)
+df.to_csv("orders_in_mail_venta.csv")
